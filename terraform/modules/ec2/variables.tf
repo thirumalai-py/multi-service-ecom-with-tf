42e639a3914bfa09dc3bbcb0d63ca82d767e300b
Thirumalai Raja <thirumalai.py@gmail.com>
2025-07-26 20:38:10 +0530

    update the variable

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,29 +1,36 @@
 variable "ami_id" {
-    type = "string"
+  description = "value of ami id"
+  type    = string
 }
 
 variable "instance_type" {
-    type = "string"
+  description = "value of instance type"
+  type    = string
+}
+
+variable "ec2_subnet_id" {
+  description = "value of subnet id"
+  type    = string
 }
 
 variable "key_name" {
-    type = "string"
+  description = "value of key name"
+  type    = string
 }
 
-variable "subnet_id" {
-    type = "string"
+variable "tag_name" {
+  description = "value of tag name"
+  type    = string
 }
 
 variable "security_groups" {
-    type = list(string)
-}
-
-variable "security_groups" {
-    type = list(string)
+  description = "value"
+  type = list(string)
 }
 
 variable "volume_size" {
-    type = "number"
+  description = "value of volume size in GB"
+  type    = number
 }
 
 variable "volume_type" {
