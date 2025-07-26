f476bbc7c8178da8e20a1f2d232a66ff8d46bacb
Thirumalai Raja <thirumalai.py@gmail.com>
2025-07-26 20:36:35 +0530

    update the variable

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,28 +1,28 @@
-variable "ami_id" = {
+variable "ami_id" {
     type = "string"
 }
 
-variable "instance_type" = {
+variable "instance_type" {
     type = "string"
 }
 
-variable "key_name" = {
+variable "key_name" {
     type = "string"
 }
 
-variable "subnet_id" = {
+variable "subnet_id" {
     type = "string"
 }
 
-variable "security_groups" = {
+variable "security_groups" {
     type = list(string)
 }
 
-variable "security_groups" = {
+variable "security_groups" {
     type = list(string)
 }
 
-variable "volume_size" = {
+variable "volume_size" {
     type = "number"
 }
 
